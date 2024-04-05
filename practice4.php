edfb39eef266f2ccc237d19d42f211d7e1bbbd3e
Vegito2367 <tejaaditya.g@gmail.com>
2024-04-05 17:37:27 -0500

    Final changes

--- practice4.php
+++ practice4.php
@@ -1,22 +1,20 @@
 <?php
 
-// Function to generate the multiplication table
 function generateMultiplicationTable($number) {
   echo "<table>";
   echo "<tr><th></th>";
 
-  // Print column indexes
+  #Column Indexes
   for ($i = 1; $i <= $number; $i++) {
     echo "<th>$i</th>";
   }
 
   echo "</tr>";
 
-  // Print table rows
   for ($i = 1; $i <= $number; $i++) {
     echo "<tr><th>$i</th>";
 
-    // Print table cells
+
     for ($j = 1; $j <= $number; $j++) {
       echo "<td>" . ($i * $j) . "</td>";
     }
@@ -28,10 +26,9 @@ function generateMultiplicationTable($number) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // Retrieve the input from the form
+
   $name = $_POST["number"];
 
-  // Process the input (in this case, just display it)
   echo "<h2>Multiplication Table for $name</h2>";
   generateMultiplicationTable($name);
 }
